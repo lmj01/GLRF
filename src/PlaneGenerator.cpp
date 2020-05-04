9c0d0205902331eaf05f4ea969be8c9659e9e5c2
Erik Wessel <44678770+DunkleMango@users.noreply.github.com>
2020-05-04 23:33:48 +0200

    fixed missplaced indices in PlaneGenerator

--- src/PlaneGenerator.cpp
+++ src/PlaneGenerator.cpp
@@ -6,7 +6,7 @@ using namespace GLRF;
 MeshData PlaneGenerator::create(glm::vec3 center, glm::vec3 normal, glm::vec3 direction, float side_length, unsigned int  tesselation, float uvScaling) {
 	MeshData data;
 	unsigned int steps = tesselation + 1;
-	float step_size = tesselation ? side_length / steps : side_length;
+	float step_size = side_length / steps;
 	float step_size_uv = 1.0f / (float)steps;
 	data.vertices = std::vector<VertexFormat>();
 	data.indices = std::vector<GLuint>();
@@ -23,22 +23,24 @@ MeshData PlaneGenerator::create(glm::vec3 center, glm::vec3 normal, glm::vec3 di
 	unsigned int steps_vertices = steps + 1;
 	for (unsigned int s = 0; s < steps_vertices; s++) {
 		for (unsigned int t = 0; t < steps_vertices; t++) {
-			glm::vec3 p = start + next_row * static_cast<float>(s) + next_column * static_cast<float>(t);
-			glm::vec2 uv = glm::vec2((float) s, (float) t) * step_size_uv;
+			float s_f = static_cast<float>(s);
+			float t_f = static_cast<float>(t);
+			glm::vec3 p = start + next_row * s_f + next_column * t_f;
+			glm::vec2 uv = glm::vec2(s_f, t_f) * step_size_uv;
 			data.vertices.push_back(VertexFormat(p, normal, uv * uvScaling, tangent));
 		}
 	}
 
 	for (unsigned int s = 0; s < steps; s++) {
 		for (unsigned int t = 0; t < steps; t++) {
-			GLuint start_idx = steps * s + t;
+			GLuint start_idx = steps_vertices * s + t;
 			data.indices.value().push_back(start_idx);
 			data.indices.value().push_back(start_idx + 1);
 			data.indices.value().push_back(start_idx + steps_vertices);
 
+			data.indices.value().push_back(start_idx + steps_vertices);
 			data.indices.value().push_back(start_idx + 1);
 			data.indices.value().push_back(start_idx + steps_vertices + 1);
-			data.indices.value().push_back(start_idx + steps_vertices);
 		}
 	}
 
